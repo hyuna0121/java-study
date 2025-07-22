484c1bf0bbb19ba658d8805f90d5cd4b06748695
hyuna0121 <teng3156@naver.com>
2025-07-22 23:51:08 +0900

    chap05 homework add

---
+++ chap05/src/homework/Homework7.java
@@ -0,0 +1,25 @@
+package homework;
+
+import java.util.Arrays;
+
+public class Homework7 {
+
+	public static void main(String[] args) {
+		 String[] report = {"Introduction", "Research", "Conclusion"};
+		 String[] arr =  Arrays.copyOf(report, report.length);
+		 arr[0] = "Team Feedback";
+		 
+		 System.out.print("원본 보고서 : ");
+		 for (String str : report) {
+			 System.out.print(str + ", ");
+		 }
+		 
+		 System.out.println();
+		 
+		 System.out.print("복사된 보고서 : ");
+		 for (String str : arr) {
+			 System.out.print(str + ", ");
+		 }
+	}
+
+}
---
+++ chap05/src/homework/Homework8.java
@@ -0,0 +1,27 @@
+package homework;
+
+import java.util.Arrays;
+
+public class Homework8 {
+
+	public static void main(String[] args) {
+		String[] menu = {"Burger", "Pizza", "Pasta"};
+		String[] add = {"Salad", "Soup"};
+		
+		String[] todayMenu  = Arrays.copyOf(menu, menu.length + add.length);
+		System.arraycopy(add, 0, todayMenu, menu.length, add.length);
+		
+		System.out.print("어제 메뉴판 : ");
+		for (String str : menu) {
+			System.out.print(str + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.print("오 메뉴판 : ");
+		for (String str : todayMenu) {
+			System.out.print(str + " ");
+		}
+	}
+
+}
