e4513053dd4a6354ecaedc608f02d7583a60f558
hyuna0121 <teng3156@naver.com>
2025-07-28 17:07:33 +0900

    chap07 sec03 exercise

---
+++ chap07/src/sec03/exam03/exercise/HttpServlet.java
@@ -0,0 +1,5 @@
+package sec03.exam03.exercise;
+
+public abstract class HttpServlet {
+	public abstract void service();
+}
---
+++ chap07/src/sec03/exam03/exercise/LoginServlet.java
@@ -0,0 +1,8 @@
+package sec03.exam03.exercise;
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	public void service() {
+		System.out.println("로그인합니다.");
+	}
+}
