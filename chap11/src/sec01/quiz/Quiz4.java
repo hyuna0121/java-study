f42077ca78bcc89a376194f3f39c713520c047ef
hyuna0121 <teng3156@naver.com>
2025-08-04 10:15:07 +0900

    chap11 sec01 example

--- chap11/src/sec01/quiz/Quiz4.java
+++ chap11/src/sec01/quiz/Quiz4.java
@@ -9,18 +9,27 @@ public class Quiz4 {
 //	전체 학생 중에서 오늘 커피 쏠 사람을 랜덤으로 뽑으려고 해요.
 //	Math.random()을 활용해서 학생 한 명을 추출하여 출력하세요.
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("학생수를 입력하세요 : ");
-		int studentNo = Integer.parseInt(sc.nextLine());
-		String[] students = new String[studentNo];
+		System.out.print("학생 수를 입력하세요 : ");
+		int length = Integer.parseInt(sc.nextLine());
+		String[] students = new String[length];
 		
-		for (int i = 0; i < studentNo; i++) {
+		for (int i = 0; i < length; i++) {
 			students[i] = sc.nextLine();
 		}
 		
-		int randomNo = (int) Math.random() * (studentNo + 1);
-		System.out.println(students[randomNo]);
+		// 공식화 : (int) (Math.random() * (최대값 - 최소값 + 1)) + 최소값
+		int randomNo = (int) (Math.random() * length);
+		
+		int count = 5;
+		while (count > 0) {
+			System.out.println(count--);
+			Thread.sleep(1000); // 현재 스레드를 일정 시간 정지
+			// 주의 : 현재 스레드를 멈추기 때문에 사용 주의!
+		}
+		
+		System.out.println("오늘 커피 쏠 사람 : " + students[randomNo]);
 	}
 }
