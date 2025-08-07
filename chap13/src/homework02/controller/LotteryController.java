ec0a1ffb4beadc947fea25e4538673020e754f83
hyuna0121 <teng3156@naver.com>
2025-08-07 17:31:14 +0900

    chap13 set homework

--- chap13/src/homework02/controller/LotteryController.java
+++ chap13/src/homework02/controller/LotteryController.java
@@ -18,10 +18,11 @@ public class LotteryController {
 	}
 	
 	public boolean deleteObject(Lottery l) {
-		// 1. 전달 받은 l을 lottery에서 삭제
-		// 2. 당첨자 목록 win중에 삭제된 추첨 대상자가 존재할 수 있음
-		// 3. 삭제 결과인 boolean 값과 win 객체가 null이 아닐 때에만
-		// win에도 해당 추첨 대상자 삭제
+		if (win.contains(l) && !win.isEmpty()) {
+			win.remove(l);
+			winObject();
+		}
+		
 		return lottery.remove(l);
 	}
 	
@@ -30,9 +31,6 @@ public class LotteryController {
 	}
 	
 	public Set<Lottery> winObject() {
-		// 만일 당첨자 목록에 삭제된 추첨 대상자가 있다면
-		// 기존에 당첨된 사람은 제외
-		// 삭제된 사람의 자리만 새로운 추첨자로 채우기
 		if (lottery.size() < 4) return null;
 		List<Lottery> lotteryList = new ArrayList<Lottery>(lottery);
 		
